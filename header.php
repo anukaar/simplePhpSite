8f22fb5ccdb29dd8fe3d9332b61f019b4ea23f3f
anukaar <150908048+anukaar@users.noreply.github.com>
2024-07-04 23:09:33 +0300

    Update header.php

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">    
 <style>
   a, .nav-link,.navbar-brand {
-    color: white;
+    color: blue;
 }
 
 h3 {
@@ -24,7 +24,7 @@ h3 {
     <div class="row">
   <nav class="navbar navbar-expand-lg" style="background-color: rgb(65, 7, 109);" >
   <div class="container-fluid">
-    <a class="navbar-brand" href="index.php">My Tasks - DKC</a>
+    <a class="navbar-brand" href="index.php">My Duties AnuK</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
